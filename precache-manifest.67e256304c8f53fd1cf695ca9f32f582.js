21aefae139789469676a1a11286fb5e3119528d3
stefanghadjiew <stefanghadjiew@abv.bg>
2020-10-16 01:54:05 +0300

    Updates

---
+++ precache-manifest.67e256304c8f53fd1cf695ca9f32f582.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6686de6d5b436e17a6fc426a88e6e6ce",
+    "url": "/Hotels/index.html"
+  },
+  {
+    "revision": "a465b4a3b84524522d15",
+    "url": "/Hotels/static/css/main.597a12e6.chunk.css"
+  },
+  {
+    "revision": "cb0f0c9a5a191a084bd5",
+    "url": "/Hotels/static/js/2.26434bf0.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Hotels/static/js/2.26434bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a465b4a3b84524522d15",
+    "url": "/Hotels/static/js/main.6d64c72c.chunk.js"
+  },
+  {
+    "revision": "faa82d7395486927768c",
+    "url": "/Hotels/static/js/runtime-main.942eedf5.js"
+  },
+  {
+    "revision": "1cfc34519d3c4cc630566ec3a20bdddb",
+    "url": "/Hotels/static/media/HotelsMain.1cfc3451.jpg"
+  },
+  {
+    "revision": "a0fb51ab4b88c00165c7592ff5465059",
+    "url": "/Hotels/static/media/homeImage.a0fb51ab.jpg"
+  },
+  {
+    "revision": "3704dccd57f26b3347a790735f8a55b9",
+    "url": "/Hotels/static/media/hotel10.3704dccd.jpg"
+  },
+  {
+    "revision": "c3dcb79afaf67b558f8794a54e42e0b7",
+    "url": "/Hotels/static/media/hotel7.c3dcb79a.jpg"
+  },
+  {
+    "revision": "47b9e3f7ae321a0581eb750ae215caf3",
+    "url": "/Hotels/static/media/hotel8.47b9e3f7.jpg"
+  },
+  {
+    "revision": "62610cdbe8713ed6344fa7d421361e15",
+    "url": "/Hotels/static/media/hotel9.62610cdb.jpg"
+  },
+  {
+    "revision": "60587ec75c56edc10a0432b92e875257",
+    "url": "/Hotels/static/media/oneOfBest1.60587ec7.jpg"
+  },
+  {
+    "revision": "7101b314c021f5e6f528e14aadf55af5",
+    "url": "/Hotels/static/media/oneOfBest2.7101b314.jpg"
+  },
+  {
+    "revision": "bb0486ff31d06fe2330d1fecf2caf636",
+    "url": "/Hotels/static/media/oneOfBest3.bb0486ff.jpg"
+  },
+  {
+    "revision": "b0c8477e160e483e65d9c90e6ded6306",
+    "url": "/Hotels/static/media/oneOfBest4.b0c8477e.jpg"
+  },
+  {
+    "revision": "ffa6b966eacfb388c209956fffb34082",
+    "url": "/Hotels/static/media/oneOfBest5.ffa6b966.jpg"
+  },
+  {
+    "revision": "837931110369e897483f5fd6b2ca5ecc",
+    "url": "/Hotels/static/media/oneOfBest6.83793111.jpg"
+  },
+  {
+    "revision": "140fbe862e7ba7bc558736ee72d64e28",
+    "url": "/Hotels/static/media/presidential1.140fbe86.jpg"
+  },
+  {
+    "revision": "42585a290aec39ede95770cb62f46327",
+    "url": "/Hotels/static/media/presidential10.42585a29.jpg"
+  },
+  {
+    "revision": "9ff0c3adc85dbf842a04fd6d5ccae88d",
+    "url": "/Hotels/static/media/presidential2.9ff0c3ad.jpg"
+  },
+  {
+    "revision": "176ab74d2bd93c8f48ad1c329bb889f5",
+    "url": "/Hotels/static/media/presidential3.176ab74d.jpg"
+  },
+  {
+    "revision": "2f3837062abf52d2aa07e9ed3f91725a",
+    "url": "/Hotels/static/media/presidential4.2f383706.jpg"
+  },
+  {
+    "revision": "d31128482cd79f5019eaedc2e92e0b6c",
+    "url": "/Hotels/static/media/presidential5.d3112848.jpg"
+  },
+  {
+    "revision": "47c9a4e09f79156710635dee5fe0b9c1",
+    "url": "/Hotels/static/media/presidential6.47c9a4e0.jpg"
+  },
+  {
+    "revision": "aa892df2739dd48cad384642b2cdcadd",
+    "url": "/Hotels/static/media/presidential7.aa892df2.jpg"
+  },
+  {
+    "revision": "cf0aeec862c7670c3ab5ab6a9052f1de",
+    "url": "/Hotels/static/media/presidential8.cf0aeec8.jpg"
+  },
+  {
+    "revision": "86f7fe1840b41c23784e45ba1f134973",
+    "url": "/Hotels/static/media/presidential9.86f7fe18.jpg"
+  },
+  {
+    "revision": "625514838e8b99de1d7f1303c0342454",
+    "url": "/Hotels/static/media/reviewer1.62551483.jpeg"
+  },
+  {
+    "revision": "e4b5e0e955d1506991e4363d59ec2c17",
+    "url": "/Hotels/static/media/reviewer2.e4b5e0e9.jpg"
+  },
+  {
+    "revision": "eaf750cf5e47b840772c9005a5275846",
+    "url": "/Hotels/static/media/reviewer3.eaf750cf.jpeg"
+  },
+  {
+    "revision": "61281ddee7ee3a4ccf909b937fc48c01",
+    "url": "/Hotels/static/media/reviewer4.61281dde.jpg"
+  },
+  {
+    "revision": "19b94b040df7c50fc9b63c6baffe16dd",
+    "url": "/Hotels/static/media/room1.19b94b04.jpg"
+  },
+  {
+    "revision": "100c5bcffd9a4c643789a7eb712563b5",
+    "url": "/Hotels/static/media/room10.100c5bcf.jpg"
+  },
+  {
+    "revision": "e560f1c1efc4dc8293f16f8e63eeb81a",
+    "url": "/Hotels/static/media/room11.e560f1c1.jpg"
+  },
+  {
+    "revision": "6ba6f70b625a4a3a962f68ccbbba480f",
+    "url": "/Hotels/static/media/room12.6ba6f70b.jpg"
+  },
+  {
+    "revision": "1eed9b02d5bf8401f44d96c1dd78600a",
+    "url": "/Hotels/static/media/room13.1eed9b02.jpg"
+  },
+  {
+    "revision": "e42d34ec249e8e535718f14138dcab22",
+    "url": "/Hotels/static/media/room14.e42d34ec.jpg"
+  },
+  {
+    "revision": "1eed9b02d5bf8401f44d96c1dd78600a",
+    "url": "/Hotels/static/media/room15.1eed9b02.jpg"
+  },
+  {
+    "revision": "fee9675872f77801cf45599fb5c5df6f",
+    "url": "/Hotels/static/media/room16.fee96758.jpg"
+  },
+  {
+    "revision": "0ef908c8bc1eef315b9f2cf3820d9a90",
+    "url": "/Hotels/static/media/room17.0ef908c8.jpg"
+  },
+  {
+    "revision": "e8009e98d8647056e7886fd151522340",
+    "url": "/Hotels/static/media/room18.e8009e98.jpg"
+  },
+  {
+    "revision": "e726484d4621f829b751ff09aca4d4a1",
+    "url": "/Hotels/static/media/room19.e726484d.jpg"
+  },
+  {
+    "revision": "488a93fc6b054770b949b0578383ac42",
+    "url": "/Hotels/static/media/room2.488a93fc.jpg"
+  },
+  {
+    "revision": "66c3816f3216664abcc59e08ed6e8079",
+    "url": "/Hotels/static/media/room20.66c3816f.jpg"
+  },
+  {
+    "revision": "c5889662da05ac2fb6f819880f1a90e2",
+    "url": "/Hotels/static/media/room21.c5889662.jpg"
+  },
+  {
+    "revision": "f5b3d85ebbe500c81a43d36449203788",
+    "url": "/Hotels/static/media/room22.f5b3d85e.jpg"
+  },
+  {
+    "revision": "130e93d86787810c8e8a885cf32619fd",
+    "url": "/Hotels/static/media/room23.130e93d8.jpg"
+  },
+  {
+    "revision": "52a7e144f2df7b0f2c06943d7b395312",
+    "url": "/Hotels/static/media/room24.52a7e144.jpg"
+  },
+  {
+    "revision": "130e93d86787810c8e8a885cf32619fd",
+    "url": "/Hotels/static/media/room25.130e93d8.jpg"
+  },
+  {
+    "revision": "8fe3784a4820da1cdfde482947d89b8b",
+    "url": "/Hotels/static/media/room26.8fe3784a.jpg"
+  },
+  {
+    "revision": "5af8fe70308806ebccd8a7784d0ebc97",
+    "url": "/Hotels/static/media/room27.5af8fe70.jpg"
+  },
+  {
+    "revision": "26980aaa2f2c9b0e2a434276de029291",
+    "url": "/Hotels/static/media/room28.26980aaa.jpg"
+  },
+  {
+    "revision": "34c9475158133be5cb946bacb5c624a3",
+    "url": "/Hotels/static/media/room29.34c94751.jpg"
+  },
+  {
+    "revision": "2d8d9bddc1dc0e3a8b56e4b8ec2b0b43",
+    "url": "/Hotels/static/media/room3.2d8d9bdd.jpg"
+  },
+  {
+    "revision": "dd0d19e83c31992979b27c4f26b357e3",
+    "url": "/Hotels/static/media/room30.dd0d19e8.jpg"
+  },
+  {
+    "revision": "a2bfa9b15976dfb6109c5ce1708048f8",
+    "url": "/Hotels/static/media/room4.a2bfa9b1.jpg"
+  },
+  {
+    "revision": "3d35e34afe3ea81b3271ea593139502f",
+    "url": "/Hotels/static/media/room5.3d35e34a.jpg"
+  },
+  {
+    "revision": "b4f78bff52b9fef6f63a56bf4ef893d8",
+    "url": "/Hotels/static/media/room6.b4f78bff.jpg"
+  },
+  {
+    "revision": "19b4f195734a105fe18d587662d96a94",
+    "url": "/Hotels/static/media/room7.19b4f195.jpg"
+  },
+  {
+    "revision": "38cbebd98a504ce1cabbf0e08f310be2",
+    "url": "/Hotels/static/media/room8.38cbebd9.jpg"
+  },
+  {
+    "revision": "e08e0dbc3bb3fa828df154a1a582bcdc",
+    "url": "/Hotels/static/media/room9.e08e0dbc.jpg"
+  }
+]);
